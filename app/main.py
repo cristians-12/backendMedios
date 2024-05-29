9f2f512f91e756fcc4092de8546ad0d9eb4c8ed0
Cristian <97061228+cristians-12@users.noreply.github.com>
2024-05-29 18:14:33 -0500

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ class Data(BaseModel):
     f: float
     o: float
     e:float
+    c: float
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,17 +27,18 @@ def obtener_parametros(data: Data):
     fa = 2 * math.pi * data.f
     permea_medio = data.u * 4 * math.pi * 1e-7
     permitividad = data.e * 8.8542*1e-12
-    permitividad_dielec = fa*permitividad*0.2*1e-3 
+    permitividad_dielec = (fa*permitividad) * 0.3 * 1e-3
+    conductividad_conductor = data.o*1e7
     
-    pen = math.sqrt(2 / (fa * permea_medio * data.o))
-    pen_str = format(pen, ".2e")
-    # return pen < data.a*1e-2
+    pen = math.sqrt(2/(fa * permea_medio * conductividad_conductor))
+    # pen_str = format(pen, ".2e")
     if pen > data.a*1e-2:
         divisor = math.log(data.b / data.a)
         if divisor != 0:
-            L = permea_medio / (4 * math.pi) + permea_medio / math.pi * (math.log(data.b / data.a))
+            # L = permea_medio / (2 * math.pi) + permea_medio / math.pi * (math.log(data.b / data.a))
+            L = permea_medio / (2 * math.pi) * (math.log(data.b/data.a)+1/4+1/(4*(data.c**2-data.b**2))*(data.b**2-3*data.c**2+4*data.c**4/(data.c**2-data.b**2)*math.log(data.c/data.b)))
             C = (2*math.pi * (permitividad)) / (math.log(data.b/data.a))
-            R = 1/(2*data.o*math.pi)*(1/data.a**2)
+            R = 1/(2*conductividad_conductor*math.pi)*(1/data.a**2+1/(data.c**2-data.b**2))
             G = (math.pi*permitividad_dielec)/(math.log(data.b/data.a))
             return {
                 'msg':'Es baja frecuencia',
@@ -44,6 +46,7 @@ def obtener_parametros(data: Data):
                 'C': format(C, ".2e"),
                 "R": format(R, ".2e"),
                 "G": format(G, ".2e"),
+                "conduc_d" : format(pen, ".2e")
             }
         else:
             return {
@@ -51,9 +54,9 @@ def obtener_parametros(data: Data):
             }
     elif pen < data.a*1e-2:
         
-        L=(permea_medio/2*math.pi)*math.log(data.b/data.a)
+        L=((permea_medio/(2*math.pi))*math.log(data.b/data.a))
         C = (2*math.pi*permitividad)/(math.log(data.b/data.a))
-        R = 1/(2*math.pi*data.a*pen*data.o)*(1/data.a+1/data.b)
+        R = (1/(2*math.pi*pen*conductividad_conductor))*(1/(data.a*1e-2)+1/(data.b*1e-2))
         G = (2*math.pi*permitividad_dielec)/(math.log(data.b/data.a))
         
         return {
@@ -62,15 +65,28 @@ def obtener_parametros(data: Data):
             'C': format(C, ".2e"),
             "R": format(R, ".2e"),
             "G": format(G, ".2e"),
+            "conduc_d" : format(permitividad_dielec, ".2e")
         }
 
 def obtener_params_placas(data:Data):
     fa = 2 * math.pi * data.f
     permea_medio = data.u * 4 * math.pi * 1e-7
     permitividad = data.e * 8.8542*1e-12
-    pen = math.sqrt(2 / (fa * permea_medio * data.o))
+    conduc_dielect = fa*permitividad*0.2*1e-3 
     
-    return {}
+    pen = math.sqrt(2 / (fa * permea_medio * data.o))
+    L = (permea_medio*data.a)/math.pi
+    C = (permitividad*data.b)/data.a
+    R = 2/(data.o*pen*data.b)
+    G = (conduc_dielect*data.b)/data.a
+    
+    return {
+        "L": format(L, ".2e"),
+        'C': format(C, ".2e"),
+        "R": format(R, ".2e"),
+        "G": format(G, ".2e"),
+        
+    }
 
 @app.get("/")
 async def root():
